35ceee14492063fb0ff99cd61b1e3935d62d81ed
brendonfm <freitas.brendon@gmail.com>
2019-08-04 18:17:51 +0100

    Set up a Spring Boot app

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,21 @@
+
 package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
-	public static void main(String[] args) {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World!";
+	}
+
+	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
